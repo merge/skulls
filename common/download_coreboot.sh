9a04b9695bce016e2d0570bd1611b7ca3b2aa74d
Martin Kepplinger <martink@posteo.de>
2018-11-05 13:47:19 +0100

    x230: bump to coreboot 715cb40963

--- common/download_coreboot.sh
+++ common/download_coreboot.sh
@@ -30,9 +30,9 @@ function gitUpdate() {
 
   # pull in microcode update to the blobs repo
   cd "$DOCKER_COREBOOT_DIR/3rdparty/blobs/" || exit
-  git pull https://review.coreboot.org/blobs refs/changes/52/27352/4
+  git pull https://review.coreboot.org/blobs refs/changes/52/27352/5
   cd "$DOCKER_COREBOOT_DIR" || exit
-  git pull https://review.coreboot.org/coreboot refs/changes/64/27864/2
+  git pull https://review.coreboot.org/coreboot refs/changes/64/27864/3
 }
 ################################################################################
 
