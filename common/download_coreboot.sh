1fdaf3797ba12580fd4e7491e4502e89ce0b4101
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2019-05-02 07:16:05 +0200

    update coreboot to current master

--- common/download_coreboot.sh
+++ common/download_coreboot.sh
@@ -32,7 +32,7 @@ function gitUpdate() {
   cd "$DOCKER_COREBOOT_DIR/3rdparty/blobs/" || exit
   git pull https://review.coreboot.org/blobs refs/changes/66/32166/1
   cd "$DOCKER_COREBOOT_DIR" || exit
-  git pull https://review.coreboot.org/coreboot refs/changes/67/32167/1
+  git pull https://review.coreboot.org/coreboot refs/changes/67/32167/3
 }
 ################################################################################
 
