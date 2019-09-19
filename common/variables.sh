0412a28456201c3049bc0418a0b4ca77828d9823
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2019-09-19 11:31:51 +0200

    update coreboot-sdk

--- common/variables.sh
+++ common/variables.sh
@@ -5,7 +5,7 @@
 ################################################################################
 ## VARIABLES
 ################################################################################
-export COREBOOT_SDK_VERSION="1.53"
+export COREBOOT_SDK_VERSION="d70f5fae1c"
 
 export DOCKER_ROOT_DIR="/home/coreboot"
 export DOCKER_SCRIPT_DIR="$DOCKER_ROOT_DIR/scripts"
